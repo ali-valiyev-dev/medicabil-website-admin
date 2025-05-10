bbc8c08dc657ce03db040b8e55bb4b3b5215a18e
Ali Valiyev <ali.valiyev.dev@gmail.com>
2025-05-10 12:34:38 +0400

    add date reset functionality and extend slider data with additional entries

--- src/pages/slider/components/Filter.vue
+++ src/pages/slider/components/Filter.vue
@@ -24,6 +24,8 @@
       class="date-input col q-mr-md q-px-md rounded-borders"
     >
       <template v-slot:append>
+        <q-icon name="cancel" class="cursor-pointer q-mr-xs" @click="form.date = null" />
+
         <q-icon name="event" color="secondary" class="cursor-pointer">
           <q-popup-proxy cover transition-show="scale" transition-hide="scale">
             <q-date v-model="form.date" mask="DD.MM.YYYY" range minimal>
