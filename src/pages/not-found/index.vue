1bb6ed00e7a270499aa00eb6d7fdb2f97aad8dd7
Ali Valiyev <ali.valiyev.dev@gmail.com>
2025-05-13 14:42:16 +0400

    add Error page

--- src/pages/not-found/index.vue
+++ src/pages/not-found/index.vue
@@ -1,27 +1,20 @@
 <template>
-  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
-    <div>
-      <div style="font-size: 30vh">
-        404
-      </div>
-
-      <div class="text-h2" style="opacity:.4">
-        Oops. Nothing here...
-      </div>
-
-      <q-btn
-        class="q-mt-xl"
-        color="white"
-        text-color="blue"
-        unelevated
-        to="/"
-        label="Go Home"
-        no-caps
-      />
+  <div class="absolute-center text-center text-grey-9">
+    <div style="font-size: 18vh">404!</div>
+    <div class="text-h4 text-bold q-mb-md">Bulunamadı.</div>
+    <div class="text-h6 text-weight-light">
+      Aradığınız sayfa Bulunamadı. taşınmış veya silinmiş olabilir.
     </div>
+
+    <q-btn
+      to="/"
+      unelevated
+      no-caps
+      no-wrap
+      padding="md xl"
+      label="Ana Sayfaya Git"
+      color="secondary"
+      class="text-body1 rounded-borders q-mt-xl"
+    />
   </div>
 </template>
-
-<script setup>
-//
-</script>
