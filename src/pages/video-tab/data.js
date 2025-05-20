cde17a6365438bd84c3dcd53095709390f4e5452
Ali Valiyev <ali.valiyev.dev@gmail.com>
2025-05-20 14:30:58 +0400

    add video tab page

---
+++ src/pages/video-tab/data.js
@@ -0,0 +1,13 @@
+export const sampleData = [
+  {
+    id: 1,
+    title: ' Özel Medicabil Hastanesi',
+    buttonTitle: 'Hastane Tanıtımı',
+    videoLink: 'https://youtu.be/NkdD23jg7kE',
+    content:
+      '154 yataklı, 27 bin 500 metrekarelik alana sahip, gelişmiş teknolojileri ve ameliyathaneleri ile modern hastanelerimizde, 24 uzmanlık alanı, 6 profesör, 2 doçent, 2 yardımcı doçent, 60 uzman, 10 pratisyen hekimimiz ve 70 hemşiremiz ile sizlere hizmet etmekten mutluluk duyuyoruz.',
+    isActive: true,
+    order: 1,
+    date: '2025-05-10',
+  },
+]
