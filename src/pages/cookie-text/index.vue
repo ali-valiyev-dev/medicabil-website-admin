fade68a046af69347160e7d2f90602321ed33db2
Ali Valiyev <ali.valiyev.dev@gmail.com>
2025-05-20 15:10:12 +0400

    update breadcrumb label

--- src/pages/cookie-text/index.vue
+++ src/pages/cookie-text/index.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page padding>
     <q-card flat class="row justify-between">
-      <PageHeader :breadcrumbs="[{ label: 'Sık Sık Sorulan Sorular' }]" />
+      <PageHeader :breadcrumbs="[{ label: 'Çerez Metini' }]" />
       <q-btn
         unelevated
         noCaps
