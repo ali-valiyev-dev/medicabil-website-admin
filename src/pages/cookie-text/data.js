94fa8ffc12041989b96af16b4ce2430a23cf162c
Ali Valiyev <ali.valiyev.dev@gmail.com>
2025-05-20 15:09:08 +0400

    add cookie text page

---
+++ src/pages/cookie-text/data.js
@@ -0,0 +1,12 @@
+export const sampleData = [
+  {
+    id: 10,
+    title: 'Cookies',
+    buttonTitle: 'I Agree',
+    targetLink: '',
+    content: `#### We and selected third parties use cookies or similar technologies for technical purposes and, with your consent, for other purposes as specified in the Cookie Policy. Denying consent may make related features unavailable.`,
+    isActive: true,
+    order: 10,
+    date: '2025-05-10',
+  },
+]
